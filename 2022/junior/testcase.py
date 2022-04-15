626ff4d3876b0784ebe654afed943883c6476a1f
kvasilev3 <vasilevk@gmail.com>
2022-04-15 00:17:57 -0400

    Code now uses original source files from ZIP.

--- 2022/junior/testcase.py
+++ 2022/junior/testcase.py
@@ -2,28 +2,22 @@ import os
 import subprocess
 import time
 
-def rename_files(problem):
-    tests_folder = './junior_data'
-    for input_filename in sorted(os.listdir("{0}/{1}".format(tests_folder, problem))):
-        if not input_filename.startswith("{0}.".format(problem)):
-            old_filename = "{0}/{1}/{2}".format(tests_folder, problem, input_filename)
-            new_filename = "{0}/{1}/{2}".format(tests_folder, problem, 'j4'+input_filename[2:])
-            os.rename(old_filename, new_filename)
-
-
 def test(problem):
     tests_folder = './junior_data'
+    tests_subfolders = {'j1':'j1', 'j2':'j2', 'j3':'j3', 'j4':'s2j4', 'j5':'j5', }
+    tests_prefixes = {'j1':'j1', 'j2':'j2', 'j3':'j3', 'j4':'s2', 'j5':'j5', }
+
     start_time = time.time()
-    files_list = os.listdir("{0}/{1}".format(tests_folder, problem))
+    files_list = os.listdir("{0}/{1}".format(tests_folder, tests_subfolders[problem]))
     files_list = sorted(files_list, key=lambda a: a if "sample" in a else "~"+a)    
     for input_filename in files_list:
-        if input_filename.startswith("{0}.".format(problem)) and input_filename.endswith(".in"):
-            inputFile = open("{0}/{1}/{2}".format(tests_folder, problem, input_filename), 'r')
+        if input_filename.startswith("{0}.".format(tests_prefixes[problem])) and input_filename.endswith(".in"):
+            inputFile = open("{0}/{1}/{2}".format(tests_folder, tests_subfolders[problem], input_filename), 'r')
             stdout_lines = subprocess.run(['python3', '{0}.py'.format(problem)], stdin=inputFile, capture_output=True).stdout.splitlines()
             stdout_lines = [l.decode('utf-8') for l in stdout_lines]
 
             output_filename = input_filename.replace('.in', '.out')
-            expected_result = open("{0}/{1}/{2}".format(tests_folder, problem, output_filename), 'r').readlines()
+            expected_result = open("{0}/{1}/{2}".format(tests_folder, tests_subfolders[problem], output_filename), 'r').readlines()
             expected_result = [l.strip() for l in expected_result]
             if len(stdout_lines) != len(expected_result):
                 print('Test {0} failed: Expected {1} lines, but got {2}.'.format(input_filename, len(expected_result), len(stdout_lines)))
@@ -40,8 +34,8 @@ def test(problem):
 
 
 if __name__ == '__main__':
-    #test('j1')
-    #test('j2')
-    #test('j3')
-    #test('j4')
-    test('j5')
+    test('j1')
+    test('j2')
+    test('j3')
+    test('j4')
+    #test('j5')
